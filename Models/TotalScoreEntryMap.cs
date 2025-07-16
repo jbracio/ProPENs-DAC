09f5a8034330544a5b4474ae6d7da3ae04c83309
johannesbracio <johannes.bracio@tum.de>
2025-07-16 14:55:24 +0200

    Deploy GitHub Pages via deploy.sh

--- Models/TotalScoreEntryMap.cs
+++ Models/TotalScoreEntryMap.cs
@@ -9,7 +9,7 @@ namespace DeutschlandDashboard.Models
             Map(m => m.County).Name("County");
             Map(m => m.TotalScore).Name("Total County Score");
             Map(m => m.WasteHeatScore).Name("Waste Heat Score");
-            Map(m => m.ElectricityScore).Name(" Cost of CO2 Score");
+            Map(m => m.ElectricityScore).Name("Cost of CO2 Score");
             Map(m => m.RenewableScore).Name("RES Score");
             Map(m => m.CO2TransportScore).Name("CO2 Transport Score");
             Map(m => m.LandUsageScore).Name("Land Usage Score");
